70f56fe78e21c8e22bd33992eb4747b2359cffdf
Levi <muhammad.levi@sid-indonesia.org>
2021-04-08 11:48:26 +0700

    Affected Issue(s): #1192322793
    What this commit has achieved:
    1. Re-generated Spring Boot App using Spring Initializr
    2. Added dependencies are: Lombok, Spring Boot DevTools, PostgreSQL Driver, JDBC API

---
+++ src/main/java/org/sidindonesia/dbconverter/DbConverterApplication.java
@@ -0,0 +1,13 @@
+package org.sidindonesia.dbconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConverterApplication.class, args);
+	}
+
+}
